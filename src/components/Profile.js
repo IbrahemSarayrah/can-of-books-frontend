3893fe1943c66157d86153d9de1d3e12e1413f2f
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-07-11 17:29:04 +0300

    lab11

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,7 @@ import { withAuth0 } from '@auth0/auth0-react';
 class Profile extends Component {
     render() {
         const { user, isAuthenticated } = this.props.auth0;
+        console.log(user);
         
         return (
             <>
@@ -11,6 +12,7 @@ class Profile extends Component {
                     <>
                         <div>Hello {user.name}</div>
                         <div>Hello {user.email}</div>
+                        <div><img src={user.picture} alt={user.name}></img></div>
                     </>
                 }
             </>
