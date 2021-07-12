6d7ec2270bfd273598da18b6ae0f9251cfeed9ec
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-07-12 20:30:14 +0300

    lab-12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import './BestBooks.css';
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bestBooks: [],
+      show: false
+    }
+  }
+  componentDidMount = async () => {
+
+    const { user } = this.props.auth0;
+
+    let bestBooks = await axios.get(`${process.env.REACT_APP_BOOK_DATA}/books?userEmail=${user.email}`)
+    await this.setState({
+      bestBooks: bestBooks.data,
+      show: true
+    })
+    console.log(bestBooks.data);
+  }
   render() {
-    return(
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
+        {this.state.show &&
+          this.state.bestBooks.map(book => {
+            return (
+              <Container>
+              <Row>
+                <Col>
+                  <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={book.img} />
+                    <Card.Body>
+                      <Card.Title>{book.name}</Card.Title>
+                      <Card.Text>
+                        {book.description}
+                      </Card.Text>
+                      <Card.Text>
+                        {book.status}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+              </Container>
+            )
+          })
+        }
       </Jumbotron>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
+
